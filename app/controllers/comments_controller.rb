68def9e6c2749a998fb301d8f3fc71a18d8a31a0
Luke Merry <lmerry@gmail.com>
2016-10-07 10:37:36 -0600

    Iterations 2 through 5

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
   before_action :set_comment, only: [:show, :edit, :update, :destroy]
-
+  before_action :set_post
   # GET /comments
   # GET /comments.json
   def index
@@ -24,15 +24,18 @@ class CommentsController < ApplicationController
   # POST /comments
   # POST /comments.json
   def create
-    @comment = Comment.new(comment_params)
+    @comment = @post.comments.new(comment_params)
+    @comment.user_id = 1
 
     respond_to do |format|
       if @comment.save
         format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
         format.json { render :show, status: :created, location: @comment }
+        format.js #create.js.erb
       else
         format.html { render :new }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
+        pry
       end
     end
   end
@@ -67,8 +70,12 @@ class CommentsController < ApplicationController
       @comment = Comment.find(params[:id])
     end
 
+    def set_post
+      @post = Post.find(params[:post_id])
+    end
+
     # Never trust parameters from the scary internet, only allow the white list through.
     def comment_params
-      params.require(:comment).permit(:post_id, :body)
+      params.require(:comment).permit(:post_id, :body, :user_id)
     end
-end
+  end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user = User.create first_name: 'Bob', last_name: 'George', handle: 'bgeorge'
+user2 = User.create first_name: 'Luke', last_name: 'Merry', handle: 'pha3l'
